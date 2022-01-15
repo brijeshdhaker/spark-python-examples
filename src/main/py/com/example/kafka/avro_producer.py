4d5a8e5c1217f9d408374523f47320dc5fcb6fb9
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2022-01-15 20:14:56 +0530

    2022-01-15 Update Docker Configurations.

--- src/main/py/com/example/kafka/avro_producer.py
+++ src/main/py/com/example/kafka/avro_producer.py
@@ -139,11 +139,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="SerializingProducer Example")
-    parser.add_argument('-b', dest="bootstrap_servers", required=True,
-                        help="Bootstrap broker(s) (host[:port])")
-    parser.add_argument('-s', dest="schema_registry", required=True,
-                        help="Schema Registry (http(s)://host[:port]")
-    parser.add_argument('-t', dest="topic", default="example_serde_avro",
-                        help="Topic name")
+    parser.add_argument('-b', dest="bootstrap_servers", required=True, help="Bootstrap broker(s) (host[:port])")
+    parser.add_argument('-s', dest="schema_registry", required=True, help="Schema Registry (http(s)://host[:port]")
+    parser.add_argument('-t', dest="topic", default="example_serde_avro", help="Topic name")
+    parser.add_argument('-g', dest="group", default="example_serde_avro", help="Consumer group")
 
     main(parser.parse_args())
