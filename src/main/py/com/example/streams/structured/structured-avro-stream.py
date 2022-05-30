712abaaa1b521cdb93107bed4f54fe9d45f2c6db
Brijesh K. Dhaker <brijeshdhaker@gmail.com>
2022-05-30 21:18:01 +0530

    2022-05-30 Add Transaction avro stream process.

---
+++ src/main/py/com/example/streams/structured/structured-avro-stream.py
@@ -0,0 +1,71 @@
+#
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import pyspark.sql.functions as fn
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from pyspark.sql.avro.functions import from_avro, to_avro
+
+spark = SparkSession \
+    .builder \
+    .master("local[4]") \
+    .appName("txn-stream-topic") \
+    .getOrCreate()
+
+spark.conf.set("spark.sql.streaming.checkpointLocation", "/home/brijeshdhaker/txn-json-stream/checkpoints/")
+spark.conf.set("spark.sql.shuffle.partitions", "1")
+spark.conf.set("spark.sql.hive.convertMetastoreParquet", "false")
+
+spark.sparkContext.setLogLevel('ERROR')
+
+binary_to_string = fn.udf(lambda x: str(int.from_bytes(x, byteorder='big')), StringType())
+
+# Subscribe to 1 topic
+structureStreamDf = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka-broker:9092") \
+    .option("subscribe", "txn-avro-stream-topic") \
+    .option("startingOffsets", "earliest")\
+    .option("failOnDataLoss", "false") \
+    .load() \
+    .withColumn('key', col("key").cast(StringType())) \
+    .withColumn('fixedValue', expr("substring(value, 6, length(value)-5)")) \
+    .withColumn('valueSchemaId', binary_to_string(expr("substring(value, 2, 4)"))) \
+    .select('topic', 'partition', 'offset', 'timestamp', 'timestampType', 'key', 'valueSchemaId', 'fixedValue')
+
+# Returns True for DataFrames that have streaming sources
+print("structureStreamDf.isStreaming : " + str(structureStreamDf.isStreaming))
+print("Schema for structureStreamDf  : ")
+structureStreamDf.printSchema()
+
+#
+# Confluent Schema Registry
+#
+schema_registry_conf = {
+    'url': 'http://schema-registry:8081',
+    'basic.auth.user.info': '{}:{}'.format('userid', 'password')
+}
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+txn_schema_response = schema_registry_client.get_latest_version("txn-avro-stream-topic-value").schema
+txn_schema = txn_schema_response.schema_str
+
+
+fromAvroOptions = {"mode": "PERMISSIVE"}
+structuredGpsDf = (
+    structureStreamDf.select(from_avro('fixedValue', txn_schema, fromAvroOptions).alias("transactions")).selectExpr("transactions.*")
+)
+
+
+# Writing to console sink (for debugging)
+structuredGpsDf.writeStream \
+    .outputMode("update") \
+    .format("console") \
+    .option("maxRows", 50) \
+    .option("truncate", False) \
+    .trigger(processingTime="5 seconds")\
+    .start() \
+    .awaitTermination()
+
+spark.stop()
---
+++ src/main/py/com/example/streams/structured/transaction_avro_stream.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python2
+
+import random
+import json
+import os
+from confluent_kafka import Producer, KafkaError
+from random import randint
+from time import sleep
+from os.path import expanduser
+from pathlib import Path
+from com.example.models.Transaction import Transaction
+from confluent_kafka import SerializingProducer
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from com.example.utils.load_avro_schema_from_file import load_avro_schema_from_file, load_avro_schema_as_str
+
+
+def txn_to_dict(user, ctx):
+    # User._address must not be serialized; omit from dict
+    return dict(
+        id=user.id,
+        uuid=user.uuid,
+        cardtype=user.cardType,
+        website=user.website,
+        product=user.product,
+        amount=user.amount,
+        city=user.city,
+        country=user.country,
+        addts=user.addts
+    )
+
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    topic = 'txn-avro-stream-topic'
+    AVRO_PATH = Path(expanduser("~"), "IdeaProjects", "spark-python-examples", "resources", "avro",
+                     "transaction-record.avsc")
+    key_schema, value_schema = load_avro_schema_from_file(AVRO_PATH)
+
+    schema_registry_conf = {'url': 'http://schema-registry:8081'}
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    avro_serializer = AvroSerializer(schema_registry_client, str(value_schema), txn_to_dict)
+
+    # Create Producer instance
+    producer_conf = {'bootstrap.servers': 'kafka-broker:9092',
+                     'key.serializer': StringSerializer('utf_8'),
+                     'value.serializer': avro_serializer}
+
+    producer = SerializingProducer(producer_conf)
+
+    # Optional per-message on_delivery handler (triggered by poll() or flush())
+    # when a message has been successfully delivered or
+    # permanently failed delivery (after retries).
+    delivered_records = 0
+    def acked(err, msg):
+        global delivered_records
+        """Delivery report handler called on
+        successful or failed delivery of message
+        """
+        if err is not None:
+            print("Failed to deliver message: {}".format(err))
+        else:
+            delivered_records += 1
+            print("Produced record to topic {} partition [{}] @ offset {}".format(msg.topic(), msg.partition(),
+                                                                                  msg.offset()))
+
+    while True:
+        # Serve on_delivery callbacks from previous calls to produce()
+        producer.poll(0.0)
+        try:
+
+            transaction = Transaction.random()
+            record_key = str(transaction.uuid)
+
+            print("Producing record: {}\t{}".format(record_key, transaction.to_dict()))
+            producer.produce(topic, key=record_key, value=transaction, on_delivery=acked)
+            sleep(1)
+
+        except KeyboardInterrupt:
+            break
+        except ValueError:
+            print("Invalid input, discarding record...")
+            continue
+
+    producer.flush()
+
+    print("{} messages were produced to topic {}!".format(delivered_records, topic))
