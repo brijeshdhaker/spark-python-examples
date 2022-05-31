ab00d181e242d04b45fa15b9d280c0c739897c80
Brijesh K. Dhaker <brijeshdhaker@gmail.com>
2022-05-31 09:05:50 +0530

    2022-05-31 Add Transaction delimiter stream process.

---
+++ src/main/py/com/example/streams/structured/structured-json-stream.py
@@ -0,0 +1,69 @@
+#
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+spark = SparkSession \
+    .builder \
+    .master("local[4]") \
+    .appName("txn-json-stream") \
+    .getOrCreate()
+
+spark.conf.set("spark.sql.streaming.checkpointLocation", "/home/brijeshdhaker/txn-json-stream/checkpoints/")
+spark.conf.set("spark.sql.shuffle.partitions", "1")
+spark.conf.set("spark.sql.hive.convertMetastoreParquet", "false")
+
+spark.sparkContext.setLogLevel('ERROR')
+
+txnSchema = StructType([
+    StructField("id", IntegerType()),
+    StructField("uuid", StringType()),
+    StructField("cardtype", StringType()),
+    StructField("website", StringType()),
+    StructField("product", StringType()),
+    StructField("amount", DoubleType()),
+    StructField("city", StringType()),
+    StructField("country", StringType()),
+    StructField("addts", TimestampType())
+])
+
+# Subscribe to 1 topic
+structureStreamDf = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka-broker:9092") \
+    .option("subscribe", "txn-json-stream-topic") \
+    .option("startingOffsets", "earliest")\
+    .option("failOnDataLoss", "false") \
+    .load() \
+    .withColumn('key', col("key").cast(StringType())) \
+    .withColumn('value', from_json(col("value").cast(StringType()), txnSchema))
+
+# Returns True for DataFrames that have streaming sources
+print("structureStreamDf.isStreaming : " + str(structureStreamDf.isStreaming))
+print("Schema for structureStreamDf  : ")
+structureStreamDf.printSchema()
+
+#resultStreamDF = structureStreamDf.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+recordsDF = structureStreamDf.select("value.*", "timestamp")
+
+# Apply watermarks on event-time columns
+recordsDFWithWatermark = recordsDF.selectExpr("id", "cardtype", "website", "product", "amount", "city", "country", "addts", "timestamp") \
+    .withWatermark("addts", "30 seconds")  \
+    .groupBy(window(recordsDF.addts, "10 seconds", "5 seconds"), recordsDF.country) \
+    .count()
+
+# Writing to console sink (for debugging)
+recordsDFWithWatermark.writeStream \
+    .outputMode("update") \
+    .format("console") \
+    .option("maxRows", 50) \
+    .option("truncate", False) \
+    .trigger(processingTime="5 seconds")\
+    .start() \
+    .awaitTermination()
+
+spark.stop()
---
+++ src/main/py/com/example/streams/structured/transaction_json_stream.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python2
+"""
+from kafka import KafkaProducer
+from random import randint
+from time import sleep
+from com.example.models.Transaction import Transaction
+import sys
+
+BROKER = 'kafka-broker:9092'
+TOPIC = 'structured-stream-source'
+
+try:
+    p = KafkaProducer(bootstrap_servers=BROKER)
+except Exception as e:
+    print("ERROR --> {e}")
+    sys.exit(1)
+
+while True:
+    transaction = Transaction.random()
+    print(">>> '{}'".format(transaction.to_dict()))
+    p.send(topic=TOPIC, value=bytes(str(transaction.to_dict()), encoding='utf8'), key=bytes(str(transaction.uuid), encoding='utf8'))
+    sleep(.1)
+"""
+
+import random
+import json
+from confluent_kafka import Producer, KafkaError
+from random import randint
+from time import sleep
+from com.example.models.Transaction import Transaction
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    topic = 'txn-json-stream-topic'
+    # Create Producer instance
+    producer_conf = {
+        'bootstrap.servers': 'kafka-broker:9092',
+    }
+    producer = Producer(producer_conf)
+    # Create topic if needed
+
+
+    delivered_records = 0
+    # Optional per-message on_delivery handler (triggered by poll() or flush())
+    # when a message has been successfully delivered or
+    # permanently failed delivery (after retries).
+    def acked(err, msg):
+        global delivered_records
+        """Delivery report handler called on
+        successful or failed delivery of message
+        """
+        if err is not None:
+            print("Failed to deliver message: {}".format(err))
+        else:
+            delivered_records += 1
+            print("Produced record to topic {} partition [{}] @ offset {}".format(msg.topic(), msg.partition(), msg.offset()))
+
+    while True:
+        # Serve on_delivery callbacks from previous calls to produce()
+        producer.poll(0.0)
+        try:
+            transaction = Transaction.random()
+            record_key = str(transaction.uuid)
+            record_value = transaction.to_json()
+            print("Producing record: {}\t{}".format(record_key, record_value))
+            producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
+            #sleep(5)
+        except KeyboardInterrupt:
+            break
+        except ValueError:
+            print("Invalid input, discarding record...")
+            continue
+
+    producer.flush()
+
+    print("{} messages were produced to topic {}!".format(delivered_records, topic))
